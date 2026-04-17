8f9fb35f956409bf5e8b15e421e53d5c2b7a6c01
giangka10052006 <giangka20061005@gmail.com>
2026-04-17 15:44:29 +0700

    Tích hợp pug

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require("express"); // nhúng giống export nhúng thư viện or nhúng file ở đây đang nhúng thư viện
+const path = require("path");
 const app = express();
 const port = 3000;
+// Thiết lập views
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
-	// get là lấy ra đường dẫn chỗ /
-	res.send("Trang chủ");
+	res.render("client/pages/home",{
+			pageTitle: "Trang chủ"
+	});
 });
 
 app.get("/tours", (req, res) => {
 	// get là lấy ra đường dẫn chỗ /
-	res.send("Danh sách tour");
+	res.render("client/pages/tour-list", {
+    pageTitle: "Danh sách tour"
+  });
 });
 
 app.listen(port, () => {
